1f96d1a5ce9076fa8e42171e3f19a475d9dba32f
Amodhyh <35001977+amodhyh@users.noreply.github.com>
2025-08-13 16:46:27 +0530

    started to implement the base structure for the jwt auth

--- src/main/java/com/yasitha/test1/Utility/JWTUtility.java
+++ src/main/java/com/yasitha/test1/Utility/JWTUtility.java
@@ -1,19 +1,61 @@
 package com.yasitha.test1.Utility;
 
+import com.yasitha.test1.Model.Role;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.security.Key;
+import java.sql.Date;
+
 public class JWTUtility {
-    // This class will contain methods for generating and validating JWT tokens.
-    public String generateToken(String username) {
-        // Logic to generate JWT token
-        return "generatedToken"; // Placeholder
+    final private Key secretKey = io.jsonwebtoken.security.Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    // 1 hour in milliseconds
+    final private Date expirationTime = new Date(System.currentTimeMillis()+ 3600000);
+
+    //Generate JWT token
+    public String generateToken(String email,Role role) {
+        return io.jsonwebtoken.Jwts.builder()
+                .setSubject(email)
+                .claim("role", role)
+                .setExpiration(expirationTime)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .signWith(secretKey)
+                .compact();
     }
 
+    // Validate JWT token
     public boolean validateToken(String token, String username) {
-        // Logic to validate JWT token
-        return true;
+        try {
+            String extractedUsername = extractDetails(token);
+            return extractedUsername.equals(username) && !isTokenExpired(token);
+
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
     }
 
-    public String  extractDetails(String token) {
-        // Logic to extract username from JWT token
-        return "extractedUsername"; // Placeholder
+    // Extract username from JWT token
+    public String extractDetails(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
     }
+
+    // Check if the token is expired
+    private boolean isTokenExpired(String token) {
+        java.util.Date expiration = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+        return expiration.before(new java.util.Date());
+    }
+}
+
+
 }
